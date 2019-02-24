397a1c9c0a53267b6848dae540ed648bb3a8fb98
WooKyoung Noh <wookay.noh@gmail.com>
2019-02-25 01:48:33 +0900

    Grid traversal functions

--- src/API.jl
+++ src/API.jl
@@ -1,7 +1,7 @@
 module API # module H3
 
 # types
-export GeoCoord, GeoBoundary
+export GeoCoord, GeoBoundary, CoordIJ
 
 # Indexing functions
 export geoToH3, h3ToGeo, h3ToGeoBoundary
@@ -10,13 +10,13 @@ export geoToH3, h3ToGeo, h3ToGeoBoundary
 export h3GetResolution, h3GetBaseCell, stringToH3, h3ToString, h3IsValid, h3IsResClassIII, h3IsPentagon
 
 # Grid traversal functions
-export kRing
+export kRing, maxKringSize, kRingDistances, hexRange, hexRangeDistances, hexRanges, hexRing, h3Line, h3LineSize, h3Distance, experimentalH3ToLocalIj, experimentalLocalIjToH3
 
 
 using ..Lib
 
 # types
-using .Lib: H3Index, GeoCoord, GeoBoundary
+using .Lib: H3Index, GeoCoord, GeoBoundary, CoordIJ
 
 
 # Indexing functions
@@ -76,11 +76,78 @@ end
 
 # Grid traversal functions
 
-function kRing(origin::H3Index, ring_size::Int)::Vector{H3Index}
-    array_len = Lib.maxKringSize(ring_size)
+function kRing(origin::H3Index, k::Int)::Vector{H3Index}
+    array_len = Lib.maxKringSize(k)
     krings = Vector{H3Index}(undef, array_len)
-    Lib.kRing(origin, ring_size, krings)
+    Lib.kRing(origin, k, krings)
     krings
+end
+
+function maxKringSize(k::Int)::Int
+    Lib.maxKringSize(k)
+end
+
+function kRingDistances(origin::H3Index, k::Int)::NamedTuple{(:out, :distances)}
+    array_len = Lib.maxKringSize(k)
+    out = Vector{H3Index}(undef, array_len)
+    distances = Vector{Cint}(undef, array_len)
+    Lib.kRingDistances(origin, k, out, distances)
+    (out = out, distances = distances)
+end
+
+function hexRange(origin::H3Index, k::Int)::Vector{H3Index}
+    array_len = Lib.maxKringSize(k)
+    out = Vector{H3Index}(undef, array_len)
+    Lib.hexRange(origin, k, out)
+    out
+end
+
+function hexRangeDistances(origin::H3Index, k::Int)::NamedTuple{(:out, :distances)}
+    array_len = Lib.maxKringSize(k)
+    out = Vector{H3Index}(undef, array_len)
+    distances = Vector{Cint}(undef, array_len)
+    Lib.hexRangeDistances(origin, k, out, distances)
+    (out = out, distances = distances)
+end
+
+function hexRanges(h3Set::Vector{H3Index}, k::Int)::Vector{H3Index}
+    array_len = Lib.maxKringSize(k)
+    out = Vector{H3Index}(undef, array_len)
+    Lib.hexRanges(h3Set, length(h3Set), k, out)
+    out
+end
+
+function hexRing(origin::H3Index, k::Int)::Vector{H3Index}
+    out = Vector{H3Index}(undef, 6k)
+    Lib.hexRing(origin, k, out)
+    out
+end
+
+function h3Line(origin::H3Index, destination::H3Index)::Vector{H3Index}
+    line_size = Lib.h3LineSize(origin, destination)
+    out = Vector{H3Index}(undef, line_size)
+    Lib.h3Line(origin, destination, out)
+    out
+end
+
+function h3LineSize(origin::H3Index, destination::H3Index)::Int
+    Lib.h3LineSize(origin, destination)
+end
+
+function h3Distance(origin::H3Index, h3::H3Index)::Int
+    Lib.h3Distance(origin, h3)
+end
+
+function experimentalH3ToLocalIj(origin::H3Index, h3::H3Index)::CoordIJ
+    refij = Ref{CoordIJ}()
+    Lib.experimentalH3ToLocalIj(origin, h3, refij)
+    refij[]
+end
+
+function experimentalLocalIjToH3(origin::H3Index, ij::CoordIJ)::H3Index
+    refh3 = Ref{H3Index}()
+    Lib.experimentalLocalIjToH3(origin, Ref(ij), refh3)
+    refh3[]
 end
 
 end # module H3.API
--- test/h3/api/traversal.jl
+++ test/h3/api/traversal.jl
@@ -1,22 +1,39 @@
 module test_h3_api_traversal
 
 using Test
-using H3.API # kRing
-#=
-maxKringSize
-kRingDistances
-hexRange
-hexRangeDistances
-hexRanges
-hexRing
-h3Line
-h3LineSize
-h3Distance
-experimentalH3ToLocalIj
-experimentalLocalIjToH3
-=#
+using H3.API # kRing maxKringSize kRingDistances hexRange hexRanges h3Line h3LineSize h3Distance experimentalH3ToLocalIj CoordIJ experimentalLocalIjToH3
 
-krings = kRing(0x085283473fffffff, 1)
-@test krings == [0x085283473fffffff, 0x085283447fffffff, 0x08528347bfffffff, 0x085283463fffffff, 0x085283477fffffff, 0x08528340ffffffff, 0x08528340bfffffff]
+@test kRing(0x085283473fffffff, 0) == [0x085283473fffffff]
+@test kRing(0x085283473fffffff, 1) == [0x085283473fffffff, 0x085283447fffffff, 0x08528347bfffffff, 0x085283463fffffff, 0x085283477fffffff, 0x08528340ffffffff, 0x08528340bfffffff]
+
+@test maxKringSize(0) == 1
+@test maxKringSize(1) == 7
+
+@test kRingDistances(0x08928308280fffff, 0) == (out = [0x08928308280fffff], distances = [0])
+@test kRingDistances(0x08928308280fffff, 1) == (out = [0x08928308280fffff, 0x08928308280bffff, 0x089283082873ffff, 0x089283082877ffff, 0x08928308283bffff, 0x089283082807ffff, 0x089283082803ffff], distances = [0, 1, 1, 1, 1, 1, 1])
+
+@test hexRange(0x085283473fffffff, 0) == [0x085283473fffffff]
+@test hexRange(0x085283473fffffff, 1) == [0x085283473fffffff, 0x085283447fffffff, 0x08528347bfffffff, 0x085283463fffffff, 0x085283477fffffff, 0x08528340ffffffff, 0x08528340bfffffff]
+
+@test hexRangeDistances(0x08928308280fffff, 0) == (out = [0x08928308280fffff], distances = [0])
+@test hexRangeDistances(0x08928308280fffff, 1) == (out = [0x08928308280fffff, 0x08928308280bffff, 0x089283082873ffff, 0x089283082877ffff, 0x08928308283bffff, 0x089283082807ffff, 0x089283082803ffff], distances = [0, 1, 1, 1, 1, 1, 1])
+
+@test hexRanges([0x08928308280fffff], 0) == [0x08928308280fffff]
+@test hexRanges([0x08928308280fffff], 1) == [0x08928308280fffff, 0x08928308280bffff, 0x089283082873ffff, 0x089283082877ffff, 0x08928308283bffff, 0x089283082807ffff, 0x089283082803ffff]
+
+@test hexRing(0x08928308280fffff, 0) == []
+@test hexRing(0x08928308280fffff, 1) == [0x089283082803ffff, 0x08928308280bffff, 0x089283082873ffff, 0x089283082877ffff, 0x08928308283bffff, 0x089283082807ffff]
+
+@test h3Line(0x08928308280fffff, 0x08928308280fffff) == [0x08928308280fffff]
+@test h3Line(0x08928308280fffff, 0x08928308283bffff) == [0x08928308280fffff, 0x08928308283bffff]
+
+@test h3LineSize(0x08928308280fffff, 0x08928308280fffff) == 1
+@test h3LineSize(0x08928308280fffff, 0x08928308283bffff) == 2
+
+@test h3Distance(0x08928308280fffff, 0x08928308280fffff) == 0
+@test h3Distance(0x08928308280fffff, 0x08928308283bffff) == 1
+
+@test experimentalH3ToLocalIj(0x08828308281fffff, 0x0882830828dfffff) == CoordIJ(393, 337)
+@test experimentalLocalIjToH3(0x08828308281fffff, CoordIJ(393, 337)) == 0x0882830828dfffff
 
 end # module test_h3_api_traversal
