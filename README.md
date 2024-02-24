5b397f9e15d0cc1a3911ef51312ed06d56bbb44f
WooKyoung Noh <wookay.noh@gmail.com>
2024-02-24 13:18:10 +0900

    update example for h3 4.1.0

--- README.md
+++ README.md
@@ -9,13 +9,13 @@
 ```julia
 using H3.API
 
-base = geoToH3(GeoCoord(deg2rad(0), deg2rad(0)), 5)
-rings = kRing(base, 1)
+base = latLngToCell(LatLng(deg2rad(0), deg2rad(0)), 5)
+rings = gridDisk(base, 1)
 
 x = Vector{Float64}()
 y = Vector{Float64}()
-for boundary in h3ToGeoBoundary.(rings), geo in boundary
-    push!(x, geo.lon)
+for boundary in cellToBoundary.(rings), geo in boundary
+    push!(x, geo.lng)
     push!(y, geo.lat)
 end
 
--- examples/plot.jl
+++ examples/plot.jl
@@ -1,12 +1,12 @@
-using H3.API # GeoCoord geoToH3 kRing h3ToGeoBoundary
+using H3.API # LatLng latLngToCell gridDisk cellToBoundary
 
-base = geoToH3(GeoCoord(deg2rad(0), deg2rad(0)), 5)
-rings = kRing(base, 1)
+base = latLngToCell(LatLng(deg2rad(0), deg2rad(0)), 5)
+rings = gridDisk(base, 1)
 
 x = Vector{Float64}()
 y = Vector{Float64}()
-for boundary in h3ToGeoBoundary.(rings), geo in boundary
-    push!(x, geo.lon) # 經度
+for boundary in cellToBoundary.(rings), geo in boundary
+    push!(x, geo.lng) # 經度
     push!(y, geo.lat) # 緯度
 end
 
