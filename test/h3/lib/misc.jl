83e7f0f2c3a5be17ffe83c88103b59b7b3e36a08
WooKyoung Noh <wookay.noh@gmail.com>
2024-02-25 09:34:07 +0900

    comment average edge lengths

--- test/h3/lib/misc.jl
+++ test/h3/lib/misc.jl
@@ -1,5 +1,6 @@
 module test_h3_lib_miscellaneous
 
+using Test
 using H3.Lib
 
 Lib.degsToRads
@@ -11,5 +12,16 @@ Lib.getHexagonEdgeLengthAvgM
 Lib.getNumCells
 Lib.getRes0Cells
 Lib.res0CellCount
+
+res = 0
+out = Ref{Cdouble}()
+Lib.getHexagonEdgeLengthAvgKm(res, out)
+@test out[] == 1107.712591 # h3 4.1.0 (Jan 19, 2023)
+# it's been changed to 1281.256011
+# Update average edge lengths (Mar 24, 2023)
+# https://github.com/uber/h3/commit/71e09dc002b211887c6db525609a449058233a71
+
+# https://github.com/uber/h3/blob/master/src/h3lib/lib/latLng.c#L311
+# https://h3geo.org/docs/core-library/restable/#edge-lengths
 
 end # module test_h3_lib_miscellaneous
